ed3708133f620b78b3a3ba95052afa9ce0bd001c
shee1998 <marywanjikumburu1@gmail.com>
2019-11-18 05:04:47 +0300

    rename article.html

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export MOVIE_API_KEY='6e618bbf6b0840d69f59b04985d67b2c'
+export NEWS_API_KEY='6e618bbf6b0840d69f59b04985d67b2c'
 export SECRET_KEY='<Wanjiku Mburu Secret Key>'
 
 python3.6 manage.py server
